7e70846d2eb2fd7d5a71f52e050c473dee04ff6c
alsongard <alsongard@gmail.com>
2024-05-07 20:21:39 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crime Report</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+        <div class="navbar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </header>
+    <section>
+        <form action="">
+            <input type="text" placeholder="Enter your name..">
+            <input type="password" placeholder="Enter your password..">
+        </form>
+    </section>
+</body>
+</html>
