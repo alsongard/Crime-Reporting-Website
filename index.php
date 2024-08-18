319e983cad4fb37002cd3f0ea353c0573c260812
alsongard <alsongard@gmail.com>
2024-08-18 15:18:08 +0300

    updated signup and login form, footer php file

--- index.php
+++ index.php
@@ -22,15 +22,22 @@
     <section id="home">
         <h1>Alson Private Police Force</h1>
         <p>Offering the best security services for our clients.</p>
-        <form action="./includes/user_login.php" method="POST">
-            <h1>Signup</h1>
-            <input type="text"   name="usr_fullname" placeholder="Enter name...">
-            <input type="email"  name="usr_email" placeholder="Enter email...">
-            <input type="number" name="usr_number" placeholder="Enter id number...">
-            <input type="number" name="usr_phonenumber" placeholder="Enter phone number...">
-            <input type="password" name="usr_passwd" placeholder="Enter password...">
-            <input type="submit" value="Create Account">
-        </form>
+        <div class="form-container">
+            <div class="button-container">
+                <button class="active" id="signup">SignUp</button>
+                <button id="login">Login</button>
+            </div>
+            <form action="./includes/user_login.php" method="POST">
+                <!-- <h1 class="title">Signup</h1> -->
+                <input class="user_field" type="text"   name="usr_fullname" placeholder="Enter name...">
+                <input class="email_field" type="email"  name="usr_email" placeholder="Enter email...">
+                <input class="id_field" type="number" name="usr_number" placeholder="Enter id number...">
+                <input class="phone_field" type="number" name="usr_phonenumber" placeholder="Enter phone number...">
+                <input class="passwd_field"  type="password" name="usr_passwd" placeholder="Enter password...">
+                <input class="passwd_confirm" type="password" name="confirm_passwd" placeholder="Confirm password...">
+                <input type="submit" value="Create Account">
+            </form>
+        </div>
     <section>
     <!-----------footer------------>
     <section></section>
@@ -64,6 +71,7 @@
         </div>
         <p>&copy; 2020 Crime Report</p>
     </footer>
+    <script src="./js/crime_report.js" type="text/javascript"></script>
 </body>
 </html>
 
---
+++ js/crime_report.js
@@ -0,0 +1,28 @@
+let signBtn = document.getElementById("signup");
+let loginBtn = document.getElementById("login");
+let user_field = document.querySelector(".user_field");
+let id_field = document.querySelector(".id_field");
+let passwd_confirm = document.querySelector(".passwd_confirm");
+let phone_field = document.querySelector(".phone_field");
+let title = document.querySelector(".title");
+let submitBtn = document.getElementById("submit");
+
+loginBtn.addEventListener("click", ()=>{
+    user_field.style.display = "none";
+    id_field.style.display = "none";
+    passwd_confirm.style.display = "none";
+    phone_field.style.display = "none";
+
+    loginBtn.style.backgroundColor = "rgba(237, 233, 230, 0.807)";
+    signBtn.style.backgroundColor = "transparent";
+    submitBtn.style.value = "Login";
+} );
+
+signBtn.addEventListener("click", ()=>{
+    user_field.style.display = "block";
+    id_field.style.display = "block";
+    passwd_confirm.style.display = "block";
+    phone_field.style.display = "block";
+    loginBtn.style.backgroundColor = "transparent";
+    signBtn.style.backgroundColor = "rgba(237, 233, 230, 0.807)";
+} )
